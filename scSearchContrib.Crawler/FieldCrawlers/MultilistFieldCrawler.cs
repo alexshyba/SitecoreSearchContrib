e42eac905bc32f7431137583e25c178e27869cfa
Nick Wesselman <nwesselman@hansondodge.com>
2012-09-29 22:57:56 -0500

    added support for accessing all values of a multi-valued field through a new property. existing Fields collection would only include the last seen value for a field.

---
+++ scSearchContrib.Crawler/FieldCrawlers/MultilistFieldCrawler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sitecore.Search.Crawlers.FieldCrawlers;
+using Sitecore.Data.Fields;
+using scSearchContrib.Searcher.Utilities;
+
+namespace scSearchContrib.Crawler.FieldCrawlers
+{
+    public class MultilistFieldCrawler : FieldCrawlerBase, IMultivaluedFieldCrawler
+    {
+        public MultilistFieldCrawler(Field field) : base(field) { }
+
+        public IEnumerable<string> GetValues()
+        {
+            if (FieldTypeManager.GetField(_field) is MultilistField)
+            {
+                MultilistField field = base._field;
+                return field.TargetIDs.Select(IdHelper.NormalizeGuid);
+            }
+            return new string[0];
+        }
+    }
+}

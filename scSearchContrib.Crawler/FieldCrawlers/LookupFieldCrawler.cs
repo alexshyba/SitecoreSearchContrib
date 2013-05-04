64f579e2f7a26382a3aab0adeff71d4a1d2efea9
Alex Shyba <alex.shyba@gmail.com>
2013-05-04 15:13:29 -0700

    Minor cleanup of the LookupFieldCrawler

--- scSearchContrib.Crawler/FieldCrawlers/LookupFieldCrawler.cs
+++ scSearchContrib.Crawler/FieldCrawlers/LookupFieldCrawler.cs
@@ -1,25 +1,34 @@
-﻿using System;
-using Sitecore.Data;
-using Sitecore.Data.Fields;
-using Sitecore.Search.Crawlers.FieldCrawlers;
-using scSearchContrib.Searcher.Utilities;
-
-namespace scSearchContrib.Crawler.FieldCrawlers
+﻿namespace scSearchContrib.Crawler.FieldCrawlers
 {
+    using scSearchContrib.Searcher.Utilities;
+
+    using Sitecore.Data;
+    using Sitecore.Data.Fields;
+    using Sitecore.Search.Crawlers.FieldCrawlers;
+
     /// <summary>
-    /// Defines a way to crawle lookup fields.
+    /// Defines a way to crawl lookup fields.
     /// </summary>
     public class LookupFieldCrawler : FieldCrawlerBase
     {
-        public LookupFieldCrawler(Field field) : base(field) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LookupFieldCrawler"/> class.
+        /// </summary>
+        /// <param name="field">
+        /// The field.
+        /// </param>
+        public LookupFieldCrawler(Field field)
+            : base(field)
+        {
+        }
 
         /// <summary>
-        /// Returns lookup field value as an item display name instead of item ID.
+        /// Returns lookup field value as normalized ID of the target item
         /// </summary>
-        /// <returns></returns>
+        /// <returns>processed field value</returns>
         public override string GetValue()
         {
-            var value = String.Empty;
+            var value = string.Empty;
 
             if (FieldTypeManager.GetField(_field) is LookupField)
             {

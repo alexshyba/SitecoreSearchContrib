596459836d719dbaec85f7d8aaf97e4ba6112020
Alex Shyba <alex.shyba@gmail.com>
2013-05-04 17:11:36 -0700

    bug nasty bug where the version coming from the index is different to the version published to the Delivery database (which occurs during the time where the database is updated, but index has not been updated).

--- scSearchContrib.Searcher/SkinnyItem.cs
+++ scSearchContrib.Searcher/SkinnyItem.cs
@@ -1,66 +1,199 @@
-﻿using System;
-using System.Collections.Generic;
-using Sitecore.Data;
-using System.Collections.Specialized;
-using Sitecore.Configuration;
-using Sitecore.Data.Items;
-using Sitecore.Search;
-
-namespace scSearchContrib.Searcher
+﻿namespace scSearchContrib.Searcher
 {
-   public class SkinnyItem
-   {
-      public SkinnyItem(string id, string language, string version, string databaseName)
-         : this(new ItemUri(ID.Parse(id), Sitecore.Globalization.Language.Parse(language), Sitecore.Data.Version.Parse(version), databaseName))
-      { }
+    using System.Collections.Generic;
+    using System.Collections.Specialized;
+    using System.Globalization;
 
-      public SkinnyItem(string itemUri)
-         : this(ItemUri.Parse(itemUri))
-      { }
+    using Sitecore.Configuration;
+    using Sitecore.Data;
+    using Sitecore.Data.Items;
+    using Sitecore.Search;
 
-      public SkinnyItem(ItemUri itemUri)
-      {
-         Fields = new NameValueCollection();
-         RenderedFields = new List<string>();
-         Uri = itemUri;
-         Fields.Add(BuiltinFields.Language, Uri.Language.Name);
-         Fields.Add(SearchFieldIDs.Version, Uri.Version.Number.ToString());
-      }
+    /// <summary>
+    /// The skinny item.
+    /// </summary>
+    public class SkinnyItem
+    {
+        #region Construction
 
-      public NameValueCollection Fields { get; set; }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SkinnyItem"/> class.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <param name="language">
+        /// The language.
+        /// </param>
+        /// <param name="version">
+        /// The version.
+        /// </param>
+        /// <param name="databaseName">
+        /// The database name.
+        /// </param>
+        public SkinnyItem(string id, string language, string version, string databaseName)
+            : this(new ItemUri(ID.Parse(id), Sitecore.Globalization.Language.Parse(language), Sitecore.Data.Version.Parse(version), databaseName))
+        {
+        }
 
-      public ItemUri Uri { get; set; }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SkinnyItem"/> class.
+        /// </summary>
+        /// <param name="itemUri">
+        /// The item uri.
+        /// </param>
+        public SkinnyItem(string itemUri)
+            : this(ItemUri.Parse(itemUri))
+        {
+        }
 
-      public List<string> RenderedFields { get; set; }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SkinnyItem"/> class.
+        /// </summary>
+        /// <param name="itemUri">
+        /// The item uri.
+        /// </param>
+        public SkinnyItem(ItemUri itemUri)
+        {
+            this.Fields = new NameValueCollection();
+            this.RenderedFields = new List<string>();
+            this.Uri = itemUri;
+            this.Fields.Add(BuiltinFields.Language, this.Uri.Language.Name);
+            this.Fields.Add(SearchFieldIDs.Version, this.Uri.Version.Number.ToString(CultureInfo.InvariantCulture));
+        }
 
-      public string ItemID  { get { return Uri.ItemID.ToString(); } }
+        #endregion
 
-      public string Name { get { return Fields[BuiltinFields.Name]; } }
+        /// <summary>
+        /// Gets or sets the fields.
+        /// </summary>
+        public NameValueCollection Fields { get; set; }
 
-      public string Version { get { return Uri.Version.Number.ToString(); } }
+        /// <summary>
+        /// Gets or sets the uri.
+        /// </summary>
+        public ItemUri Uri { get; set; }
 
-      public string Language { get { return Uri.Language.Name; } }
+        /// <summary>
+        /// Gets or sets the rendered fields.
+        /// </summary>
+        public List<string> RenderedFields { get; set; }
 
-      public string TemplateName { get { return Fields[SearchFieldIDs.TemplateName]; } }
+        /// <summary>
+        /// Gets the item id.
+        /// </summary>
+        public string ItemID
+        {
+            get
+            {
+                return this.Uri.ItemID.ToString();
+            }
+        }
 
-      public string Path { get { return Fields[SearchFieldIDs.FullContentPath]; } }
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return this.Fields[BuiltinFields.Name];
+            }
+        }
 
-      public Item GetItem()
-      {
-         var db = Factory.GetDatabase(Uri.DatabaseName);
-         return db != null ? db.GetItem(Uri.ItemID, Uri.Language, Uri.Version) : null;
-      }
+        /// <summary>
+        /// Gets the version.
+        /// </summary>
+        public string Version
+        {
+            get
+            {
+                return this.Uri.Version.Number.ToString(CultureInfo.InvariantCulture);
+            }
+        }
 
-      public override string ToString()
-      {
-         var itemInformation = String.Format("{0}, {1}, {2}", Uri.ItemID, Uri.Language, Uri.Version);
+        /// <summary>
+        /// Gets the language.
+        /// </summary>
+        public string Language
+        {
+            get
+            {
+                return this.Uri.Language.Name;
+            }
+        }
 
-         foreach (string key in Fields.Keys)
-         {
-            itemInformation += ", " + Fields[key];
-         }
+        /// <summary>
+        /// Gets the template name.
+        /// </summary>
+        public string TemplateName
+        {
+            get
+            {
+                return this.Fields[SearchFieldIDs.TemplateName];
+            }
+        }
 
-         return itemInformation;
-      }
-   }
+        /// <summary>
+        /// Gets the path.
+        /// </summary>
+        public string Path
+        {
+            get
+            {
+                return this.Fields[SearchFieldIDs.FullContentPath];
+            }
+        }
+
+        #region Methods
+
+        /// <summary>
+        /// The get item.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Item"/>.
+        /// </returns>
+        public Item GetItem()
+        {
+            var db = Factory.GetDatabase(this.Uri.DatabaseName, false);
+            if (db == null)
+            {
+                return null;
+            }
+
+            var versionItem = db.GetItem(this.Uri.ToDataUri());
+
+            // handling edge case when an item was published but not updated in the index
+            if (versionItem.Fields.Count > 0)
+            {
+                return versionItem;
+            }
+
+            return db.GetItem(this.Uri.ItemID, this.Uri.Language);
+        }
+
+        #endregion
+
+        #region Overrides
+
+        /// <summary>
+        /// The to string.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        public override string ToString()
+        {
+            var itemInformation = string.Format("{0}, {1}, {2}", this.Uri.ItemID, this.Uri.Language, this.Uri.Version);
+
+            foreach (string key in this.Fields.Keys)
+            {
+                itemInformation += ", " + this.Fields[key];
+            }
+
+            return itemInformation;
+        }
+
+        #endregion
+    }
 }
--- scSearchContrib.Searcher/Utilities/SearchHelper.cs
+++ scSearchContrib.Searcher/Utilities/SearchHelper.cs
@@ -1,37 +1,63 @@
-﻿#region Usings
-
-using System.Collections.Generic;
-using System;
-using System.Linq;
-using Lucene.Net.Documents;
-using Sitecore;
-using Sitecore.Data;
-using Sitecore.Data.Items;
-using Sitecore.Search;
-using scSearchContrib.Searcher.Parameters;
-
-#endregion
-
-namespace scSearchContrib.Searcher.Utilities
+﻿namespace scSearchContrib.Searcher.Utilities
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Lucene.Net.Documents;
+
+    using scSearchContrib.Searcher.Parameters;
+
+    using Sitecore;
+    using Sitecore.Data;
+    using Sitecore.Data.Items;
+    using Sitecore.Search;
+
+    /// <summary>
+    /// The search helper.
+    /// </summary>
     public class SearchHelper
     {
+        /// <summary>
+        /// Gets the context database.
+        /// </summary>
         public static Database ContextDB
         {
             get { return Context.ContentDatabase ?? Context.Database; }
         }
 
+        /// <summary>
+        /// The get item list from information collection.
+        /// </summary>
+        /// <param name="skinnyItems">
+        /// The skinny items.
+        /// </param>
+        /// <returns>
+        /// The <see cref="List"/>.
+        /// </returns>
         public static List<Item> GetItemListFromInformationCollection(List<SkinnyItem> skinnyItems)
         {
             return skinnyItems.Select(skinnyItem => skinnyItem.GetItem()).Where(i => i != null).ToList();
         }
 
+        /// <summary>
+        /// The get items from search result.
+        /// </summary>
+        /// <param name="searchResults">
+        /// The search results.
+        /// </param>
+        /// <param name="items">
+        /// The items.
+        /// </param>
+        /// <param name="showAllVersions">
+        /// The show all versions.
+        /// </param>
         public static void GetItemsFromSearchResult(IEnumerable<SearchResult> searchResults, List<SkinnyItem> items, bool showAllVersions)
         {
             foreach (var result in searchResults)
             {
                 var uriField = result.Document.GetField(BuiltinFields.Url);
-                if (uriField != null && !String.IsNullOrEmpty(uriField.StringValue()))
+                if (uriField != null && !string.IsNullOrEmpty(uriField.StringValue()))
                 {
                     var itemUri = new ItemUri(uriField.StringValue());
 
@@ -46,21 +72,35 @@ namespace scSearchContrib.Searcher.Utilities
                 }
 
                 if (showAllVersions)
+                {
                     GetItemsFromSearchResult(result.Subresults, items, true);
+                }
             }
         }
 
+        /// <summary>
+        /// The create refinements.
+        /// </summary>
+        /// <param name="fieldName">
+        /// The field name.
+        /// </param>
+        /// <param name="fieldValue">
+        /// The field value.
+        /// </param>
+        /// <returns>
+        /// The IEnumerable of Refinements.
+        /// </returns>
         public static IEnumerable<MultiFieldSearchParam.Refinement> CreateRefinements(string fieldName, string fieldValue)
         {
             var refinements = new List<MultiFieldSearchParam.Refinement>();
 
-            if (!String.IsNullOrEmpty(fieldValue) && !String.IsNullOrEmpty(fieldValue))
+            if (!string.IsNullOrEmpty(fieldName) &&
+                !string.IsNullOrEmpty(fieldValue))
             {
                 if (fieldName.Contains("|"))
                 {
-                   var fieldNames = fieldName.Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
-
-                   refinements.AddRange(fieldNames.Select(name => new MultiFieldSearchParam.Refinement(name, fieldValue)));
+                    var fieldNames = fieldName.Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+                    refinements.AddRange(fieldNames.Select(name => new MultiFieldSearchParam.Refinement(name, fieldValue)));
                 }
                 else
                 {

e07a1294a79b36beaee05451700aa5dbc28d3edb
David Petrson <dpeterson@isitedesign.com>
2013-05-14 21:50:30 -0400

    Initial commit for search tracing.

---
+++ scSearchContrib.Profiler/UI/Resources/LuceneProfiler.js
@@ -0,0 +1,17 @@
+﻿var $j = jQuery.noConflict();
+var profiler = profiler || {};
+
+profiler.load = (function (data) {
+    $j("#traceTemplate").tmpl(data).appendTo("#traceTable");
+});
+   
+profiler.toggle = (function(container) {
+    console.log("click");
+
+    if ($j(container).is(":visible"))
+        $j(container).hide();
+    else
+        $j(container).show();
+
+    return false;
+});
